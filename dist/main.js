1cac5b81b1f140cf69e7506cc56c0c965f19551f
Jimenez Cohen, Francisco Javier (Externos - AcidLabs) <franciscojavier.jimenezcohen@externos-cl.cencosud.com>
2023-12-01 18:05:39 -0500

    add : se inserta concepto de router-controller en proyecto

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const routing_controllers_1 = require("routing-controllers");
+const pos_controller_1 = require("./controllers/pos.controller");
+const app = (0, routing_controllers_1.createExpressServer)({
+    controllers: [pos_controller_1.UserController],
+});
+app.listen(3000, () => {
+    console.log(`Running on port 3000`);
+});
+//# sourceMappingURL=main.js.map
